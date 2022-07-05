febba17365e421d138b795dd5ee7047059014c4e
Pere Villega <pvillega@aracon.com>
2022-07-05 15:16:58 +0200

    Add quote
    Signed-off-by: Pere Villega <pvillega@aracon.com>

--- src/main/scala/pirate/core/Quotes.scala
+++ src/main/scala/pirate/core/Quotes.scala
@@ -5,7 +5,8 @@ import Constants.CharacterName
 object Quotes {
   val quotes: Map[CharacterName, String] = Map(
     CharacterName.Dougie -> "Dave, where can I find an organogram?",
-    CharacterName.Shah   -> "Fine! I wont call people cowboys in public channels again!"
+    CharacterName.Shah   -> "Fine! I wont call people cowboys in public channels again!",
+    CharacterName.Pere   -> "Not Elm, please..."
   )
   def get(characterName: CharacterName): String =
     quotes.getOrElse(characterName, "I'm sorry Dave, I'm afraid I can't do that.")
