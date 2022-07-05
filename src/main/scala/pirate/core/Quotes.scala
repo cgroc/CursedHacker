0bbd3648319401cbcb812ab3cd86c9d38a0ac22d
shahsara <13497500+ShahOdin@users.noreply.github.com>
2022-07-05 13:55:59 +0100

    add shah's quote

--- src/main/scala/pirate/core/Quotes.scala
+++ src/main/scala/pirate/core/Quotes.scala
@@ -4,7 +4,8 @@ import Constants.CharacterName
 
 object Quotes {
   val quotes: Map[CharacterName, String] = Map(
-    CharacterName.Dougie -> "Dave, where can I find an organogram?"
+    CharacterName.Dougie -> "Dave, where can I find an organogram?",
+    CharacterName.Shah   -> "Fine! I wont call people cowboys in public channels again!"
   )
   def get(characterName: CharacterName): String =
     quotes.getOrElse(characterName, "I'm sorry Dave, I'm afraid I can't do that.")
